98de0c4c817e56c0d9e4c67e6de62046c7febe57
Siva prakash <30351639+Sivaprakash22@users.noreply.github.com>
2019-06-15 18:16:11 +0530

    Add files via upload

---
+++ BubbleSort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int ar[100],num,index1,index2,indi,temp;
+    
+    printf("Enter the number of Elements\n");
+    scanf("%d",&num);
+    printf("Enter the number of Elements in Array\n");
+    for(indi=0;indi<num;indi++)
+    scanf("%d ",&ar[indi]);
+
+
+
+    for(index1=0;index1<num;index1++)
+    {
+        for(index2=0;index2<num-1;index2++)
+        {
+            if(ar[index2]>ar[index2+1])
+           {
+           temp=ar[index2];
+           ar[index2]=ar[index2+1];
+           ar[index2+1]=temp;
+           }
+        }
+    }
+
+
+
+
+
+
+
+
+    printf("Sorted Elements in Array\n");
+    for(indi=0;indi<num;indi++)
+    printf("%d ",ar[indi]);
+
+
+}
